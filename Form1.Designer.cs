9ee7c7bf2092fa09391357fc5eb083639ef39455
chenzhuangyi <chenzhuangyi@hotmail.com>
2026-09-13 05:14:51 +0800

    #0 定时工具（关机/锁屏）可用初版

---
+++ Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace TimerTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.shutdown = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timeTextBox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.btn_lock = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // shutdown
+            // 
+            this.shutdown.Location = new System.Drawing.Point(267, 45);
+            this.shutdown.Name = "shutdown";
+            this.shutdown.Size = new System.Drawing.Size(77, 25);
+            this.shutdown.TabIndex = 1;
+            this.shutdown.Text = "设置关机";
+            this.shutdown.UseVisualStyleBackColor = true;
+            this.shutdown.Click += new System.EventHandler(this.shutdown_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(196, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(150, 138);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 26);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "取消功能退出";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(167, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 12);
+            this.label3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(86, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "时";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(146, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "分";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(148, 11);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 12);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "当前时间:";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timeTextBox
+            // 
+            this.timeTextBox.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.timeTextBox.Location = new System.Drawing.Point(205, 4);
+            this.timeTextBox.Name = "timeTextBox";
+            this.timeTextBox.ReadOnly = true;
+            this.timeTextBox.Size = new System.Drawing.Size(189, 26);
+            this.timeTextBox.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(208, 52);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 12);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "秒后关机";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(48, 48);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            72,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(35, 21);
+            this.numericUpDown1.TabIndex = 21;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(107, 48);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(35, 21);
+            this.numericUpDown2.TabIndex = 22;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(167, 48);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(35, 21);
+            this.numericUpDown3.TabIndex = 23;
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "定时关机工具";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 26);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(100, 22);
+            this.toolStripMenuItem1.Text = "退出";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.Name = "menuItem1";
+            this.menuItem1.Size = new System.Drawing.Size(32, 19);
+            // 
+            // menuItem2
+            // 
+            this.menuItem2.Name = "menuItem2";
+            this.menuItem2.Size = new System.Drawing.Size(32, 19);
+            // 
+            // btn_lock
+            // 
+            this.btn_lock.Location = new System.Drawing.Point(267, 81);
+            this.btn_lock.Name = "btn_lock";
+            this.btn_lock.Size = new System.Drawing.Size(77, 25);
+            this.btn_lock.TabIndex = 24;
+            this.btn_lock.Text = "静音锁屏";
+            this.btn_lock.UseVisualStyleBackColor = true;
+            this.btn_lock.Click += new System.EventHandler(this.btn_lock_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(414, 176);
+            this.Controls.Add(this.btn_lock);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.timeTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.shutdown);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "定时工具（关机/锁屏）";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+        private System.Windows.Forms.Button shutdown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox timeTextBox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ToolStripMenuItem menuItem1;
+        private System.Windows.Forms.ToolStripMenuItem menuItem2;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private string shutdown_btn_state_off;
+        private string shutdown_btn_state_on;
+        private Button btn_lock;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace TimerTool
+{
+    public partial class Form1 : Form
+    {
+        // 倒计时内存状态，避免依赖UI控件读取造成错乱
+        private int _shutdownLeftSec = 0;
+        private int _lockLeftSec = 0;
+        private bool _isShutdownRunning = false;
+        private bool _isLockRunning = false;
+
+        // ==========Win32 API 用来检测MessageBox弹窗是否存在==========
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        /// <summary>检测指定标题的MessageBox弹窗是否打开，MessageBox类名固定 #32770</summary>
+        private bool IsMessageBoxOpened(string msgBoxTitle)
+        {
+            IntPtr hWnd = FindWindow("#32770", msgBoxTitle);
+            return hWnd != IntPtr.Zero;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>设置系统静音/取消静音，CoreAudio底层API，无第三方依赖 Win10/11</summary>
+        private void SetSystemMute(bool mute)
+        {
+            string cmd = mute
+                ? @"Add-Type -TypeDefinition @'using System.Runtime.InteropServices;[Guid(""5CDF2C82-841E-4546-9722-0CF74078229A""), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]interface IAudioEndpointVolume{int SetMute([MarshalAs(UnmanagedType.Bool)] bool bMute, System.Guid pguidEventContext);}[Guid(""D666063F-1587-4E43-81F1-B948E807363F""), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]interface IMMDevice{int Activate(ref System.Guid id, int clsCtx, IntPtr activationParams, out IAudioEndpointVolume endpoint);}[Guid(""A95664D2-9614-4F35-A746-DE8DB63617E6""), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]interface IMMDeviceEnumerator{int GetDefaultAudioEndpoint(int dataFlow, int role, out IMMDevice device);}public static class Audio{public static void SetMute(bool mute){var enumeratorType = Type.GetTypeFromCLSID(new System.Guid(""BCDE0395-E52F-467C-8E3D-C4579291692E""));var enumerator = (IMMDeviceEnumerator)Activator.CreateInstance(enumeratorType);enumerator.GetDefaultAudioEndpoint(0, 0, out IMMDevice dev);var g = typeof(IAudioEndpointVolume).GUID;dev.Activate(ref g, 1, IntPtr.Zero, out IAudioEndpointVolume vol);vol.SetMute(mute, System.Guid.Empty);}}'@;[Audio]::SetMute($true)"
+                : @"Add-Type -TypeDefinition @'using System.Runtime.InteropServices;[Guid(""5CDF2C82-841E-4546-9722-0CF74078229A""), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]interface IAudioEndpointVolume{int SetMute([MarshalAs(UnmanagedType.Bool)] bool bMute, System.Guid pguidEventContext);}[Guid(""D666063F-1587-4E43-81F1-B948E807363F""), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]interface IMMDevice{int Activate(ref System.Guid id, int clsCtx, IntPtr activationParams, out IAudioEndpointVolume endpoint);}[Guid(""A95664D2-9614-4F35-A746-DE8DB63617E6""), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]interface IMMDeviceEnumerator{int GetDefaultAudioEndpoint(int dataFlow, int role, out IMMDevice device);}public static class Audio{public static void SetMute(bool mute){var enumeratorType = Type.GetTypeFromCLSID(new System.Guid(""BCDE0395-E52F-467C-8E3D-C4579291692E""));var enumerator = (IMMDeviceEnumerator)Activator.CreateInstance(enumeratorType);enumerator.GetDefaultAudioEndpoint(0, 0, out IMMDevice dev);var g = typeof(IAudioEndpointVolume).GUID;dev.Activate(ref g, 1, IntPtr.Zero, out IAudioEndpointVolume vol);vol.SetMute(mute, System.Guid.Empty);}}'@;[Audio]::SetMute($false)";
+
+            try
+            {
+                using (Process ps = new Process())
+                {
+                    ps.StartInfo.FileName = "powershell.exe";
+                    ps.StartInfo.Arguments = $"-Command \"{cmd}\"";
+                    ps.StartInfo.UseShellExecute = false;
+                    ps.StartInfo.CreateNoWindow = true;
+                    ps.Start();
+                    ps.WaitForExit(1000);
+                }
+            }
+            catch
+            {
+                //异常静默处理
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeTextBox.Text = DateTime.Now.ToString("yyyy‑MM‑dd HH:mm:ss");
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            // 处理定时关机
+            if (_isShutdownRunning)
+            {
+                _shutdownLeftSec--;
+
+                //====这里判断是否运行MessageBox====
+                // ⚠注意：原生MessageBox.Show()模态阻塞，弹窗弹出后timer直接停止执行，下面API只有弹窗关闭后才会执行到
+                bool msgBoxOpen = IsMessageBoxOpened("提醒");
+                if (msgBoxOpen)
+                {
+                    //MessageBox弹窗正在打开状态
+                    Console.WriteLine("检测到MessageBox弹窗打开");
+                }
+                else
+                {
+                    //MessageBox已经关闭
+                }
+                //示例：剩余10秒弹出提醒【警告：原生MessageBox会卡住计时器！】
+                //if (_shutdownLeftSec == 10)
+                //{
+                //    MessageBox.Show("即将关机！剩余10秒","提醒");
+                //}
+
+                if (_shutdownLeftSec <= 0)
+                {
+                    _isShutdownRunning = false;
+                    timer2.Enabled = _isLockRunning;
+                    Process.Start("shutdown", "/s /t 0");
+                    return;
+                }
+                TimeSpan ts = TimeSpan.FromSeconds(_shutdownLeftSec);
+                numericUpDown1.Value = ts.Hours;
+                numericUpDown2.Value = ts.Minutes;
+                numericUpDown3.Value = ts.Seconds;
+            }
+
+            // 处理定时静音锁屏
+            if (_isLockRunning)
+            {
+                _lockLeftSec--;
+
+                bool msgBoxOpen = IsMessageBoxOpened("提醒");
+                if (msgBoxOpen)
+                {
+                    //MessageBox弹窗正在打开状态
+                    Console.WriteLine("检测到MessageBox弹窗打开");
+                }
+                else
+                {
+                    //MessageBox已经关闭
+                }
+                //示例：剩余10秒弹出提醒【警告：原生MessageBox会卡住计时器！】
+                //if (_lockLeftSec == 10)
+                //{
+                //    MessageBox.Show("即将静音锁屏！剩余10秒","提醒");
+                //}
+
+                if (_lockLeftSec <= 0)
+                {
+                    _isLockRunning = false;
+                    timer2.Enabled = _isShutdownRunning;
+                    btn_lock.Text = "静音锁屏";
+                    SetSystemMute(true);
+                    Process.Start("rundll32.exe", "user32.dll,LockWorkStation");
+                    return;
+                }
+                TimeSpan ts = TimeSpan.FromSeconds(_lockLeftSec);
+                numericUpDown1.Value = ts.Hours;
+                numericUpDown2.Value = ts.Minutes;
+                numericUpDown3.Value = ts.Seconds;
+            }
+        }
+
+        private void shutdown_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("on shutdown_Click");
+            if (shutdown.Text == "设置关机")
+            {
+                int hours = (int)numericUpDown1.Value;
+                int minutes = (int)numericUpDown2.Value;
+                int seconds = (int)numericUpDown3.Value;
+                int total = hours * 3600 + minutes * 60 + seconds;
+
+                if (total == 0)
+                {
+                    IsShutdownNow isShutdownNow = new IsShutdownNow();
+                    isShutdownNow.ShowDialog();
+                    return;
+                }
+
+                _shutdownLeftSec = total;
+                _isShutdownRunning = true;
+                timer2.Enabled = true;
+                shutdown.Text = "取消关机";
+                MessageBox.Show($"电脑将于 {total}s 后关机", "提醒");
+            }
+            else
+            {
+                _isShutdownRunning = false;
+                _shutdownLeftSec = 0;
+                shutdown.Text = "设置关机";
+                timer2.Enabled = _isLockRunning;
+
+                numericUpDown1.Enabled = true;
+                numericUpDown2.Enabled = true;
+                numericUpDown3.Enabled = true;
+            }
+        }
+
+        private void btn_lock_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("on btn_lock_Click " + btn_lock.Text);
+            if (btn_lock.Text == "静音锁屏")
+            {
+                int hours = (int)numericUpDown1.Value;
+                int minutes = (int)numericUpDown2.Value;
+                int seconds = (int)numericUpDown3.Value;
+                int total = hours * 3600 + minutes * 60 + seconds;
+
+                if (total <= 0)
+                {
+                    // 立即执行：静音+锁屏
+                    SetSystemMute(true);
+                    Process.Start("rundll32.exe", "user32.dll,LockWorkStation");
+                    return;
+                }
+
+                _lockLeftSec = total;
+                _isLockRunning = true;
+                timer2.Enabled = true;
+                btn_lock.Text = "取消锁屏";
+                MessageBox.Show($"电脑将于 {total}s 后静音锁屏", "提醒");
+            }
+            else
+            {
+                _isLockRunning = false;
+                _lockLeftSec = 0;
+                btn_lock.Text = "静音锁屏";
+                timer2.Enabled = _isShutdownRunning;
+
+                numericUpDown1.Enabled = true;
+                numericUpDown2.Enabled = true;
+                numericUpDown3.Enabled = true;
+            }
+        }
+
+        private void shutdown_cancel_Click(object sender, EventArgs e)
+        {
+            _isShutdownRunning = false;
+            _shutdownLeftSec = 0;
+            shutdown.Text = "设置关机";
+            timer2.Enabled = _isLockRunning;
+
+            numericUpDown1.Enabled = true;
+            numericUpDown2.Enabled = true;
+            numericUpDown3.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //====下面是自动生成的空事件，保留不删除，防止设计器报错====
+        private void label1_Click(object sender, EventArgs e) { }
+        private void label5_Click(object sender, EventArgs e) { }
+        private void label7_Click(object sender, EventArgs e) { }
+        private void label9_Click(object sender, EventArgs e) { }
+        private void textBox2_TextChanged(object sender, EventArgs e) { }
+        private void textBox5_TextChanged(object sender, EventArgs e) { }
+        private void textBox6_TextChanged(object sender, EventArgs e) { }
+        private void wifitext_TextChanged(object sender, EventArgs e) { }
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e) { }
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e) { }
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e) { }
+        private void textBox2_TextChanged_1(object sender, EventArgs e) { }
+    }
+}
